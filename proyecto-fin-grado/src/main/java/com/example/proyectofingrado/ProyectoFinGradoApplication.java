aaaca404ef7562b3964223e3921b7c50102aa026
AndiiYeizker <andreagwonrey@gmail.com>
2024-02-23 14:14:42 +0100

    proyecto base

---
+++ proyecto-fin-grado/src/main/java/com/example/proyectofingrado/ProyectoFinGradoApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectofingrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinGradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinGradoApplication.class, args);
+	}
+
+}
---
+++ proyecto-fin-grado/src/test/java/com/example/proyectofingrado/ProyectoFinGradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectofingrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinGradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
