dc879199840141f767ffcf7ffff6c0f04f735212
AndiiYeizker <andreagwonrey@gmail.com>
2024-02-29 13:49:05 +0100

    solucionado problema dependencias

--- proyecto-fin-grado/src/main/java/com/example/proyectofingrado/config/SpringSecurity.java
+++ proyecto-fin-grado/src/main/java/com/example/proyectofingrado/config/SpringSecurity.java
@@ -1,9 +1,10 @@
 package com.example.proyectofingrado.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security
-@Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@Configuration
+@EnableWebSecurity
 public class SpringSecurity {
 
 
